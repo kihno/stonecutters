3bfbe2061c0fabde6dbe3499404de15a8db2a448
kihno <stistyle13@gmail.com>
2022-09-23 12:12:21 -0700

    Add anonymouse messages to index page

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const Message = require('../models/message');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,7 +9,11 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/stonecutters', function(req, res, next) {
-  res.render('index', { title: 'Stonecutters' });
-})
+  Message.find().sort({ timestamp: -1 }).exec((err, results) => {
+    if (err) { return next(err) }
+
+    res.render('index', { title: 'Stonecutters', messages: results });
+  });
+});
 
 module.exports = router;
